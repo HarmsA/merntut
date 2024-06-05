ba2d1256bdf92df5f488602c309125f54354c0ff
aharms <harms2a@gmail.com>
2024-06-05 15:00:27 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  const y = 15
+  return (
+    <h2 className="text-3xl font-bold underline">
+      Hello world! {y}
+    </h2>
+  )
+}
